06ad65d2d1a15b61f82268665c4fbf57a2544dec
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-10-07 12:12:25 -0300

    Bootstrap

--- Portfolio/obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f1be18814a0cb263fbaa70da9df25f9e4aa5348")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa9ab69224e75a40e2cb99886bf4895ccbb6b110")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
