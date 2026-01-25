fcf9e91b60a141bd527604da1bff551c434edd6c
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2026-01-25 11:57:53 -0300

    Adicionando responsividade

--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9633fb7b0c533cf70dfe79541209ad5f7e0147f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb3d2fd4b60adb3f8dc93c8a558c9c89698f062c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
