59babbe8e09125d04ebf128977e9d5dfe67fafcb
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2026-01-28 19:33:53 -0300

    Ajustando Design

--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80c5143e0587dac29107aaff7e1d74b25c26f84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b310fb8ac1e32a7db05abd8347690bfe587397d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
