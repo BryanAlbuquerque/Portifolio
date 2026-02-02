f107d7dfd8830823feab7d9b94dc806f0a4104ae
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2026-02-02 19:25:54 -0300

    Nova versão com Favicon e Logo

--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e80d933215cdb1f2048b6cea4dcbcb1a44a62f7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edf2b6598aca3c53a20cd4457a72b33d81d618f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
