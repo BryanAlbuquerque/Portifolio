33016d272982481fea177eadb9ed52ea776724dc
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2026-04-01 22:04:53 -0300

    Subindo nova versão AWS

--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2687b89d5c83a5dfc62f64f44925260ca21d42e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3183f401c58c48e408c9b0c9c85af0f79b00e77a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Portfolio/obj/Release/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Release/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13134c7f2b195b0ff0eb35463bab094b18de3213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3183f401c58c48e408c9b0c9c85af0f79b00e77a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
