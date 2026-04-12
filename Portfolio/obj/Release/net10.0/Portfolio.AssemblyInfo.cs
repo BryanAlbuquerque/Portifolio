604c020371721aa61c31eb956d4180380a3b76d3
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2026-04-12 14:33:23 -0300

    Configuração final para rodar API

--- Portfolio/obj/Release/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Release/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33016d272982481fea177eadb9ed52ea776724dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2db19a81d947d6a555581aa139e85644d41f897")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
