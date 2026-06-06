9c4032f767080e8ed17ac69fe599cf7ab5148931
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2026-06-05 21:26:09 -0300

    Publicado Versão 10

--- Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b35cbbf7213b9aab2ed9426637473b29d7bf6233")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed24d35f560c02510932c6696ce08e453583d4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
